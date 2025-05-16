ef04528a92bf20a0dc962660a2baafed9f08417b
9876543214 <[ulrikmjones@gmail.com]>
2025-05-16 21:01:33 +0200

    path component

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,8 @@
+<script>
+export default {
+  name: "AboutPage",
+};
+</script>
 <template>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -9,7 +14,8 @@
     href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
     rel="stylesheet"
   />
-  <nav />
+  <Nav />
+  <location />
 </template>
 
 <style>
